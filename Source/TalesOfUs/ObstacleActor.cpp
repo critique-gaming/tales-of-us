697008b70e7340e83927a53e00b20aa0719b0c25
CristiP97 <37183815+CristiP97@users.noreply.github.com>
2022-10-02 10:40:44 +0300

    Add VFX for squish + fix castle

--- Source/TalesOfUs/ObstacleActor.cpp
+++ Source/TalesOfUs/ObstacleActor.cpp
@@ -3,6 +3,17 @@
 #include "RythmGameState.h"
 #include "TweenerSubsystem.h"
 #include "Tween.h"
+#include "Components/SceneComponent.h"
+#include "NiagaraComponent.h"
+
+AObstacleActor::AObstacleActor()
+{
+	DummyRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DummyRoot"));
+	RootComponent = DummyRoot;
+
+	VFXComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("VFXComponent"));
+	VFXComponent->SetupAttachment(DummyRoot);
+}
 
 void AObstacleActor::BeginPlay()
 {
@@ -11,6 +22,8 @@ void AObstacleActor::BeginPlay()
 	if (ARythmGameState* GameState = Cast<ARythmGameState>(GetWorld()->GetGameState())) {
 		GameState->Obstacles.Add(this);
 	}
+
+	VFXComponent->SetAsset(SquishVFX);
 }
 
 void AObstacleActor::Squish()
@@ -22,12 +35,16 @@ void AObstacleActor::Squish()
 		UGameplayStatics::PlaySound2D(this, SquishSFX);
 	}
 
+	if (SquishVFX != nullptr) {
+		VFXComponent->Activate();
+	}
+
 	FVector NewScale = GetActorScale3D();
 	NewScale.Z = 0.0f;
 	UTweenerSubsystem* TweenSubsystem = GetWorld()->GetGameInstance()->GetSubsystem<UTweenerSubsystem>();
 	UTween* NewTween = UTween::ActorScaleTo(this, NewScale, false,  0.25, EEaseType::QuarticEaseOut, ELoopType::None, 0, 0.0f, GetWorld());
 	TweenSubsystem->StartTween(NewTween);
-	
+
 	UE_LOG(LogTemp, Display, TEXT("Squish"));
 }
 
